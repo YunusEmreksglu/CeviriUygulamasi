1e8d17af4bec86ad48e6803a1f28005caa0110e8
YunusEmreksglu <yunusemre.ksglu@gmail.com>
2023-02-07 15:20:51 +0300

    kütüphane

---
+++ Kütüphane/DilKutuphanesi.py
@@ -0,0 +1,14 @@
+def CeviriDilBilgileri():
+    return [["jpn","ja","J",0],["eng","en","E",1]]
+def DilAyarlari(Value):
+    Array=[
+        ["Durdurma","Başlatma","Ayarlar","Kapatma","Çeviri",[
+            "Japonca",
+            "İngilizce"],"tr"],
+        
+        ["停止","開始","設定","シャットダウン","翻訳",[
+            "日本語",
+            "英語"],"ja"]
+
+    ]
+    return Array[Value]
